fe8810d8de81e89eee6a8dbed64346f9c4d99161
a <a>
2021-02-08 16:15:30 -0300

    iniciando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+  const [series, updateseries] = useState([])
+
+  const maxCharacters = 1500;
+  const timestamp = '1612809355974';
+  const apikeypublic = '9b5e74ce98ec30bbc45ad821262a4232';
+  const privatekey = '45cc216d4d7420f6d2ccc86d395f04f3dc58a03c';
+  const md5 = '6e7a7242dbbe651f497592dc785f8510';
+
+  const offset = Math.floor((Math.random() * maxCharacters) + 1);
+
+  useEffect(()=> {
+    axios.get(`http://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${apikeypublic}&hash=${md5}&limit=9`)
+    .then(resp =>{ 
+      updateseries(resp.data)
+      console.log(series)
+    })
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      
     </div>
   );
 }
---
+++ src/component/Nav/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Nav.css'
+
+
+export default props =>{
+    return(
+        <>
+            <div>
+                <ul>
+                    <li><a>Inicio</a></li>
+                    <li><a>Comics</a></li>
+                </ul>
+            </div>
+
+            <div class="buttom-menu">
+
+            </div>
+        </>
+    );
+}
