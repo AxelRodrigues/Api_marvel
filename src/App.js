64aefd955340e5ed8bfa14b44196f1db5903b17e
a <a>
2021-02-08 17:29:48 -0300

    atualizações

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import {useState, useEffect} from 'react'
 import axios from 'axios'
+const dotenv = require('dotenv');
 
 function App() {
-  const [series, updateseries] = useState([])
 
-  const maxCharacters = 1500;
+  const env = dotenv.config().parsed;
+  const [characters, updateseries] = useState([])
+
   const timestamp = '1612809355974';
-  const apikeypublic = '9b5e74ce98ec30bbc45ad821262a4232';
-  const privatekey = '45cc216d4d7420f6d2ccc86d395f04f3dc58a03c';
-  const md5 = '6e7a7242dbbe651f497592dc785f8510';
-
-  const offset = Math.floor((Math.random() * maxCharacters) + 1);
+  const apikeypublic = process.env.REACT_APP_APIKEYPUBLIC;
+  const privatekey = process.env.REACT_APP_PRIVATEKEY;
+  const md5 = process.env.REACT_APP_MD5;
 
   useEffect(()=> {
     axios.get(`http://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${apikeypublic}&hash=${md5}&limit=9`)
     .then(resp =>{ 
       updateseries(resp.data)
-      console.log(series)
+      console.log(characters)
     })
   })
   return (
