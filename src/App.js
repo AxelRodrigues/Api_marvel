9aa0685f81b0fe92265fd247d93df8507f869276
AxelRodrigues <>
2021-02-09 12:39:25 -0300

    manutencao get api

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ const dotenv = require('dotenv');
 function App() {
 
   const env = dotenv.config().parsed;
-  const [characters, updateseries] = useState([])
+  const [characters, updatecharacters] = useState([])
 
-  const timestamp = '1612809355974';
+  const timestamp = '1612883502';
   const apikeypublic = process.env.REACT_APP_APIKEYPUBLIC;
   const privatekey = process.env.REACT_APP_PRIVATEKEY;
   const md5 = process.env.REACT_APP_MD5;
@@ -15,8 +15,8 @@ function App() {
   useEffect(()=> {
     axios.get(`http://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${apikeypublic}&hash=${md5}&limit=9`)
     .then(resp =>{ 
-      updateseries(resp.data)
-      console.log(characters)
+      updatecharacters(resp.data)
+      console.log(characters.data.results)
     })
   })
   return (
